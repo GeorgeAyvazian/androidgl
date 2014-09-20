fd1634254045e32021d722349ccf4f5d72380276
george <ga111122222@gmail.com>
2014-09-19 23:59:13 -0700

    working setup of android opengl es 2.0 project

---
+++ src/com/example/myapp/MyActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MyActivity extends Activity {
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+        final ActivityManager systemService = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = systemService.getDeviceConfigurationInfo();
+        boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x2_000 || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 && (Build.FINGERPRINT.startsWith("generic") || Build.FINGERPRINT.startsWith("unknown") || Build.MODEL.contains("google_sdk") || Build.MODEL.contains("Emulator") || Build.MODEL.contains("Android SDK built for x86")));
+        if (supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(new GLRenderer());
+            rendererSet = true;
+        }
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
+}
