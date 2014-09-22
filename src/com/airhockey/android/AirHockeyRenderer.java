cc9a741b0c673ba100dac5658a0a0acc87ea6de1
george <ga111122222@gmail.com>
2014-09-21 23:41:57 -0700

    orthographic projections

--- src/com/airhockey/android/AirHockeyRenderer.java
+++ src/com/airhockey/android/AirHockeyRenderer.java
@@ -29,16 +29,19 @@ import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glValidateProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.orthoM;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     private static final int BYTES_PER_FLOAT = Float.SIZE / 8;
     private static final String A_COLOR = "a_Color";
     private static final String A_POSITION = "a_Position";
+    private static final String U_MATRIX = "u_Matrix";
     private final FloatBuffer vertexData;
     private final Context context;
     private int program;
@@ -48,6 +51,8 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
+    private final float[] projectionMatrix = new float[16];
+    private int uMatrixLocation;
 
     public AirHockeyRenderer(Context context) {
         this.context = context;
@@ -55,20 +60,20 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
                 // Order of coordinates: X, Y, R, G, B
 
                 // Triangle Fan
-                 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                 0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                 0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                 0.0f,  0.0f, 1.0f, 1.0f, 1.0f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                 0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                 0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
 
                 // Line 1
                 -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
                  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
 
                 // Mallets
-                 0.0f, -0.25f, 0.0f, 0.0f, 1.0f,
-                 0.0f, 0.25f, 1.0f, 0.0f, 0.0f
+                 0.0f, -0.4f, 0.0f, 0.0f, 1.0f,
+                 0.0f,  0.4f, 1.0f, 0.0f, 0.0f
         };
         vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer();
         vertexData.put(tableVertices);
@@ -101,16 +106,27 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         vertexData.position(POSITION_COMPONENT_COUNT);
         glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
         glEnableVertexAttribArray(aColorLocation);
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         glViewport(0, 0, width, height);
+        final boolean isWidthLarger = width > height;
+        final float aspectRatio = isWidthLarger ? (float) width / height : (float) height / width;
+        if (isWidthLarger) {
+            // Landscape
+            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1.0f, 1.0f, -1.0f, 1.0f);
+        } else {
+            // Portrait
+            orthoM(projectionMatrix, 0, -1.0f, 1.0f, -aspectRatio, aspectRatio, -1.0f, 1.0f);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         glClear(GL_COLOR_BUFFER_BIT);
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
         // Draw the two triangles
         glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
         // then draw the dividing line
